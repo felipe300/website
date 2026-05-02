b184887e352c1d8b75c85105d793c6b3dd19161d
Felipe Gutiérrez <caracoloro@gmail.com>
2026-05-02 18:29:19 -0400

    feat(css): add hacker background layaout

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,8 @@
 // index.ts
 var letters = "ABCDEÉFGHIJKLMNOPQRSTUVWXYZ";
+var chars = letters + "ABCDEF0123456789";
 var h1 = document.querySelector("h1");
+var bgLayer = document.getElementById("hacker-layer");
 h1.onmouseover = (event) => {
   const target = event.target;
   if (!target.dataset.value)
@@ -41,5 +43,25 @@ var setupClickToCopy = (selector, isEmail = false) => {
     }
   });
 };
+var generateText = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const fontSize = 10;
+  const columns = Math.ceil(width / (fontSize * 0.6));
+  const rows = Math.ceil(height / fontSize);
+  const totalChars = columns * rows * 1.2;
+  let str = "";
+  for (let i = 0;i < totalChars; i++) {
+    str += chars[Math.floor(Math.random() * chars.length)];
+  }
+  bgLayer.innerText = str;
+};
+generateText();
+setInterval(generateText, 70);
+window.addEventListener("resize", generateText);
+window.addEventListener("mousemove", (e) => {
+  document.documentElement.style.setProperty("--mouse-x", `${e.clientX}px`);
+  document.documentElement.style.setProperty("--mouse-y", `${e.clientY}px`);
+});
 setupClickToCopy('a[href^="mailto:"]', true);
 setupClickToCopy('a[href*="github.com"]');
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 const letters = "ABCDEÉFGHIJKLMNOPQRSTUVWXYZ";
+const chars = letters + "ABCDEF0123456789";
 const h1 = document.querySelector("h1") as HTMLElement;
+const bgLayer = document.getElementById("hacker-layer") as HTMLElement;
 
 h1.onmouseover = (event: MouseEvent) => {
   const target = event.target as HTMLElement;
@@ -50,6 +52,32 @@ const setupClickToCopy = (selector: string, isEmail: boolean = false) => {
     }
   });
 };
+
+const generateText = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  const fontSize = 10;
+  const columns = Math.ceil(width / (fontSize * 0.6));
+  const rows = Math.ceil(height / fontSize);
+  const totalChars = columns * rows * 1.2;
+
+  let str = "";
+  for (let i = 0; i < totalChars; i++) {
+    str += chars[Math.floor(Math.random() * chars.length)];
+  }
+  bgLayer.innerText = str;
+};
+
+generateText();
+setInterval(generateText, 70);
+
+window.addEventListener("resize", generateText);
+
+window.addEventListener("mousemove", (e) => {
+  document.documentElement.style.setProperty("--mouse-x", `${e.clientX}px`);
+  document.documentElement.style.setProperty("--mouse-y", `${e.clientY}px`);
+});
 
 setupClickToCopy('a[href^="mailto:"]', true);
 setupClickToCopy('a[href*="github.com"]');
